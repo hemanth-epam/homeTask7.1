2c26265b48cc744f011bd8cc039a37b8d98409ac
Hemanth Reddy <Hemanth_Reddy_Sambavaram@epam.com>
2022-07-06 13:36:41 +0530

    updated

---
+++ src/app/cli/cli.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cli',
+  templateUrl: './cli.component.html',
+  styleUrls: ['./cli.component.css']
+})
+export class CliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  clickFn() {
+    alert("First Component Clicked !")
+  }
+}
---
+++ src/app/manual/manual.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-manual',
+    templateUrl: './manual.component.html',
+    styleUrls: ['./manual.component.css']
+})
+export class ManualComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    clickFn() {
+        alert("Second Component Clicked !")
+    }
+}
